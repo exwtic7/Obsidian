b0c0426a604ed8ec4246fecc0fe46fffe12cef01
exwtic7 <fwchr0me12@gmail.com>
2025-11-16 21:23:02 +0545

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -49,7 +49,7 @@ do
     ThemeManager.BuiltInThemes = {
         ["Default"] = {
             1,
-            { FontColor = "ffffff", MainColor = "ffffff", AccentColor = "6d6d6d", BackgroundColor = "0f0f0f", OutlineColor = "282828" },
+            { FontColor = "ffffff", MainColor = "ffffff", AccentColor = "2b2b2b", BackgroundColor = "0f0f0f", OutlineColor = "282828" },
         },
         ["BBot"] = {
             2,
